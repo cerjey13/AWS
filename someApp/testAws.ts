b262da73028275c876653a7f8bb517df9298fa1a
cerjey13 <franber.cba@gmail.com>
2021-08-25 13:20:20 -0400

    new AWS projects

---
+++ someApp/testAws.ts
@@ -0,0 +1,11 @@
+var AWS = require("aws-sdk");
+// import * as AWS from "aws-sdk";
+
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", AWS.config.credentials.accessKeyId);
+    console.log("region: ", AWS.config.region);
+  }
+});
